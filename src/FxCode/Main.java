6767c75ebcfe055a0f32658a26c435870a1fd438
beanice <beanice@beany>
2022-09-08 18:40:24 +0300

    First eclipse commit

--- src/FxCode/Main.java
+++ src/FxCode/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("SplashScreenFXML.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("DashBoardFXML.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         Image icon = new Image("Images/Logo.png");
